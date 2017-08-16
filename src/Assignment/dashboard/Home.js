290af56cc620b0ddcd923837fdd27d8bdf1f7526
Lokesh Chandawar <lokesh.chandawar@harbingergroup.com>
2017-08-16 17:19:33 +0530

    new changes

--- src/Assignment/dashboard/Home.js
+++ src/Assignment/dashboard/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import isFunction from 'lodash/isFunction'
 
 class Home extends Component {
   render() {
-    const name = localStorage.getItem('userName');
+    const name = isFunction(window.GEFunctions) && window.GEFunctions.getUserName();
     return (
       <div className="App-page">
         <h2>Welcome {name}</h2>
--- src/Assignment/index.js
+++ src/Assignment/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import isObject from 'lodash/isObject'
 import isFunction from 'lodash/isFunction'
-import has from 'lodash/has'
 import get from 'lodash/get'
 import Header from './common/Header'
 import Navigation from './common/Navigation'
@@ -21,30 +20,21 @@ class Assignment extends Component {
   }
 
   redirectToMain = (event) => {
-    console.log('Event Object : ', event);
-      if (!has(event, 'data.data')) {
-        const { data } = event;
-        if (data !== "" && !isObject(data)) {
-          const eventData = JSON.parse(data);
-          const { appId, token, appUrl } = eventData
-          isFunction(window.GEFunctions) && window.GEFunctions.init(token, appId);
-          console.log('token value :', token,' app=', appId);
-          this.props.history.replace(appUrl);
-          // this.getUserList(token, appId, appUrl);
-        }
-      }
+    const { data } = event;
+    console.log(data);
+    if (data !== "" && !isObject(data)) {
+      isFunction(window.GEFunctions) && window.GEFunctions.init(data);
+      this.getUserList();
+    }
   }
 
-  getUserList = (appUrl) => {
+  getUserList = () => {
     const { history } = this.props
     isFunction(window.GEFunctions) && window.GEFunctions.getUserData()
       .then(function (e) {
         const response = JSON.parse(e.target.response);
-        console.log('Result ', response);
-        localStorage.setItem('userName', get(response, 'Data.userName', null));
-        const roleName = response['Data']['role'][0]['roleName']
-        localStorage.setItem('role', roleName === 'admin' ? roleName : 'role');
-        history.replace(appUrl);
+        window.GEFunctions.setUserName(get(response, 'Data.userName', null));
+        history.replace(window.GEFunctions.getAppUrl());
       }, function (e) {
         // handle errors
         console.log('Hello error ', e);
