547707fbea0ff639b17600ed15b934b92a95a86c
Lokesh Chandawar <lokesh.chandawar@harbingergroup.com>
2017-08-17 17:01:50 +0530

    misc changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import isFunction from 'lodash/isFunction'
 
 export default class Main extends Component {
   render() {
+    const userName = isFunction(window.GEFunctions) ? window.GEFunctions.getUserName() : ""
     return (
       <div className="App-page">
-        <h2>{'Main'}</h2>
+        <h2>{`Welcome ${userName}`}</h2>
       </div>
     )
   }
